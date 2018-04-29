bc0200ecce95132315b6906edb18b89dee252281
hyunwoo.shim <shim.hw0810@gmail.com>
2018-04-29 14:13:22 +0900

    Add AppCompatActivityExt func

---
+++ app/src/main/java/xyz/laziness/dailycommit/utils/extensions/AppCompatActivityExt.kt
@@ -0,0 +1,23 @@
+package xyz.laziness.dailycommit.utils.extensions
+
+import android.support.annotation.IdRes
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+
+
+fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, @IdRes frameId: Int) {
+    supportFragmentManager.transact {
+        replace(frameId, fragment)
+    }
+}
+
+/**
+ * Runs a FragmentTransaction, then calls commit().
+ */
+private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
+    beginTransaction().apply {
+        action()
+    }.commit()
+}
