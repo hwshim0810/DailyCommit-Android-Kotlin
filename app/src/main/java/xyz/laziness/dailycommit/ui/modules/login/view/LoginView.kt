065c324a8d50ccd3596ea049bc2b1ada0329dc6f
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-01 20:13:54 +0900

    Update LoginView

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/view/LoginView.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/view/LoginView.kt
@@ -1,2 +1,21 @@
 package xyz.laziness.dailycommit.ui.modules.login.view
 
+import xyz.laziness.dailycommit.ui.base.view.BaseView
+import xyz.laziness.dailycommit.utils.AppConstants
+
+
+interface LoginView : BaseView {
+
+    fun openLoginChooseFragment()
+
+    fun openBasicLoginFragment()
+
+    fun openTokenLoginFragment()
+
+    fun openWebLoginFragment()
+
+    fun openLoginInputFragment(loginMethod: AppConstants.LoginMethod)
+
+    fun startMainActivity()
+
+}
