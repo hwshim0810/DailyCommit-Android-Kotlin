1a38227669052453f5af65458f79301aa8986292
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-01 19:13:33 +0900

    Fix AppComponent import

--- app/src/main/java/xyz/laziness/dailycommit/DailyCommitApplication.kt
+++ app/src/main/java/xyz/laziness/dailycommit/DailyCommitApplication.kt
@@ -4,19 +4,20 @@ import android.app.Activity
 import android.app.Application
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import xyz.laziness.dailycommit.di.component.DaggerAppComponent
 import javax.inject.Inject
 
 
 class DailyCommitApplication : Application(), HasActivityInjector {
 
     @Inject
-    private lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+    internal lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
     override fun activityInjector() = activityInjector
 
     override fun onCreate() {
         super.onCreate()
-        DaggerAppComponent().builder()
+        DaggerAppComponent.builder()
                 .application(this)
                 .build()
                 .inject(this)
