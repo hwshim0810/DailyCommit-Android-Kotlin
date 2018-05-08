55bc78fd6fd0a0744e8c30163ce0f85f2e9970db
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-08 22:03:59 +0900

    Move MainActivity to module package

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/view/MainActivity.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/view/MainActivity.kt
@@ -1,25 +1,32 @@
-package xyz.laziness.dailycommit.ui.modules.main
+package xyz.laziness.dailycommit.ui.modules.main.view
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.laziness.dailycommit.R
+import xyz.laziness.dailycommit.ui.base.view.BaseActivity
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), MainView, HasSupportFragmentInjector {
+
+    @Inject
+    internal lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentDispatchingAndroidInjector
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_user -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_friends -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_settings -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,7 +34,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
---
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/view/MainView.kt
@@ -0,0 +1,6 @@
+package xyz.laziness.dailycommit.ui.modules.main.view
+
+import xyz.laziness.dailycommit.ui.base.view.BaseView
+
+
+interface MainView : BaseView
