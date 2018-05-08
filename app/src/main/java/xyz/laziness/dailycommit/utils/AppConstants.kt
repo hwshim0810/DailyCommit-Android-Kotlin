71609b9e4810f93f13a63f5b865c34226dfb3378
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-08 21:43:34 +0900

    Modified AppConstants - Modified LoginMethod enum - Add ErrorCode, REGEX

--- app/src/main/java/xyz/laziness/dailycommit/utils/AppConstants.kt
+++ app/src/main/java/xyz/laziness/dailycommit/utils/AppConstants.kt
@@ -1,5 +1,7 @@
 package xyz.laziness.dailycommit.utils
 
+import java.util.regex.Pattern
+
 
 object AppConstants {
 
@@ -8,11 +10,18 @@ object AppConstants {
         LOGIN_GITHUB(1)
     }
 
-    enum class LoginMethod {
-        BASIC,
-        TOKEN,
-        WEB
+    enum class LoginMethod constructor(val state: Int) {
+        BASIC(0),
+        TOKEN(1),
+        WEB(2)
     }
 
     internal const val PREF_NAME = "daily_pref"
+
+    internal const val EMPTY_USERNAME_ERROR = 10
+    internal const val INVALID_EMAIL_ERROR = 11
+    internal const val EMPTY_SECRET_ERROR = 12
+    internal const val LOGIN_FAILURE_ERROR = 13
+
+    internal val EMAIL_REGEX_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
 }
