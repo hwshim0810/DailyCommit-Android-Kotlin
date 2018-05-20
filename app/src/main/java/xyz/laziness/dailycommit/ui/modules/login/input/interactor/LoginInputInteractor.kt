089ecff15558f156ecae5a19d45aad947417f3b8
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-20 17:44:43 +0900

    Update Login interactor

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/input/interactor/LoginInputInteractor.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/input/interactor/LoginInputInteractor.kt
@@ -1,6 +1,15 @@
 package xyz.laziness.dailycommit.ui.modules.login.input.interactor
 
+import io.reactivex.Observable
+import xyz.laziness.dailycommit.data.network.github.response.LoginResponse
 import xyz.laziness.dailycommit.ui.base.interactor.BaseInteractor
+import xyz.laziness.dailycommit.utils.AppConstants
 
 
-interface LoginInputInteractor : BaseInteractor
+interface LoginInputInteractor : BaseInteractor {
+
+    fun doServerBasicLoginApiCall(userName: String, password: String): Observable<LoginResponse>
+
+    fun updateUserInfoInPreference(loginResponse: LoginResponse, loginMethod: AppConstants.LoginMethod)
+
+}
--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/input/interactor/LoginInputInteractorImpl.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/input/interactor/LoginInputInteractorImpl.kt
@@ -1,10 +1,24 @@
 package xyz.laziness.dailycommit.ui.modules.login.input.interactor
 
-import android.content.Context
+import io.reactivex.Observable
+import xyz.laziness.dailycommit.data.network.github.GitHubApi
+import xyz.laziness.dailycommit.data.network.github.response.LoginResponse
+import xyz.laziness.dailycommit.data.preference.BasePreference
 import xyz.laziness.dailycommit.ui.base.interactor.BaseInteractorImpl
-import xyz.laziness.dailycommit.ui.modules.login.choice.interactor.LoginChoiceInteractor
+import xyz.laziness.dailycommit.utils.AppConstants
 import javax.inject.Inject
 
 
 class LoginInputInteractorImpl
-@Inject constructor(private val context: Context) : BaseInteractorImpl(), LoginInputInteractor
+@Inject internal constructor(appPreference: BasePreference, apiHelper: GitHubApi): BaseInteractorImpl(appPreference, apiHelper), LoginInputInteractor {
+
+    override fun doServerBasicLoginApiCall(userName: String, password: String): Observable<LoginResponse> =
+        apiHelper.doServerBasicLoginApiCall(userName, password)
+
+    override fun updateUserInfoInPreference(loginResponse: LoginResponse, loginMethod: AppConstants.LoginMethod) =
+            appPreference.let {
+                it.setCurrentUserToken(loginResponse.token)
+                it.setCurrentLoginMethod(loginMethod)
+                it.setCurrentLoginState(AppConstants.LoginState.LOGIN_GITHUB)
+            }
+}
