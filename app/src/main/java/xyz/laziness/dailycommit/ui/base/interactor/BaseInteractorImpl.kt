b0c363593f8dd4a88c7e7b70287d6de239f958d0
hyunwoo.shim <shim.hw0810@gmail.com>
2018-05-20 17:43:17 +0900

    Update baseInteractor constructors

--- app/src/main/java/xyz/laziness/dailycommit/ui/base/interactor/BaseInteractorImpl.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/base/interactor/BaseInteractorImpl.kt
@@ -1,19 +1,28 @@
 package xyz.laziness.dailycommit.ui.base.interactor
 
+import xyz.laziness.dailycommit.data.network.github.GitHubApi
 import xyz.laziness.dailycommit.data.preference.BasePreference
 import xyz.laziness.dailycommit.utils.AppConstants
-import javax.inject.Inject
 
 
-open class BaseInteractorImpl : BaseInteractor {
+open class BaseInteractorImpl() : BaseInteractor {
 
-    @Inject
-    internal lateinit var appPreference: BasePreference
+    lateinit var appPreference: BasePreference
+    lateinit var apiHelper: GitHubApi
+
+    constructor(appPreference: BasePreference, apiHelper: GitHubApi) : this() {
+        this.appPreference = appPreference
+        this.apiHelper = apiHelper
+    }
+
+    constructor(appPreference: BasePreference) : this() {
+        this.appPreference = appPreference
+    }
 
     override fun isLogin(): Boolean = this.appPreference.getCurrentLoginState() != AppConstants.LoginState.LOGOUT.state
 
     override fun logout() = this.appPreference.let {
-        it.setCurrentUserId(null)
+        it.setCurrentUserToken(null)
         it.setCurrentLoginState(AppConstants.LoginState.LOGOUT)
     }
 
