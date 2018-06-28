3d30ea35594ca114d3d55c9582bec9cceb3886ff
hyunwoo.shim <shim.hw0810@gmail.com>
2018-06-29 00:48:26 +0900

    Add contribution method to Interactor, Colors constants

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractorImpl.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractorImpl.kt
@@ -1,12 +1,32 @@
 package xyz.laziness.dailycommit.ui.modules.main.friends.interactor
 
+import io.reactivex.Single
 import xyz.laziness.dailycommit.data.network.github.GitHubApi
+import xyz.laziness.dailycommit.data.network.github.data.ContributionDay
+import xyz.laziness.dailycommit.data.parser.ContributionParser
 import xyz.laziness.dailycommit.data.preference.BasePreference
 import xyz.laziness.dailycommit.ui.base.interactor.BaseInteractorImpl
+import xyz.laziness.dailycommit.ui.custom.GitHubGraphView
+import xyz.laziness.dailycommit.utils.Colors
 import javax.inject.Inject
 
 
 class FriendsStatusInteractorImpl
 @Inject internal constructor(appPreference: BasePreference, apiHelper: GitHubApi): BaseInteractorImpl(appPreference, apiHelper), FriendsStatusInteractor {
+
+    private val baseBlockColor = Colors.baseColors[appPreference.getBaseBlockColor()]
+
+    override fun doMyContributionRequest(contributionView: GitHubGraphView): Single<List<ContributionDay>> {
+        contributionView.setBaseBlockColor(baseBlockColor)
+
+        return appPreference.getCurrentUserName().run {
+            apiHelper.doContributionRequest(this)
+                    .flatMapIterable { it.parse().select(ContributionParser.targetElement) }
+                    .filter { ContributionParser.hasAttributes(it) }
+                    .map { ContributionParser.parse(it) }
+                    .toList()
+        }
+    }
+
 
 }
---
+++ app/src/main/java/xyz/laziness/dailycommit/utils/Colors.kt
@@ -0,0 +1,12 @@
+package xyz.laziness.dailycommit.utils
+
+
+object Colors {
+
+    const val DEFAULTS = "DEFAULTS"
+
+    val baseColors = hashMapOf(
+            DEFAULTS to listOfNotNull("#EBEDF0", "#239A3B", "#C6E48B", "7BC96F", "#196127")
+    )
+
+}
