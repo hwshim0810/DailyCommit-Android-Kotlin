4f82ade10e5bca7e46b1bde06ecf0323906d5fc0
hyunwoo.shim <shim.hw0810@gmail.com>
2018-06-30 14:08:04 +0900

    Add contribution request, db request to FriendsInteractor

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractor.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractor.kt
@@ -1,13 +1,18 @@
 package xyz.laziness.dailycommit.ui.modules.main.friends.interactor
 
+import io.reactivex.Observable
 import io.reactivex.Single
+import xyz.laziness.dailycommit.data.database.repository.friends.Friend
 import xyz.laziness.dailycommit.data.network.github.data.ContributionDay
 import xyz.laziness.dailycommit.ui.base.interactor.BaseInteractor
-import xyz.laziness.dailycommit.ui.custom.GitHubGraphView
 
 
 interface FriendsStatusInteractor : BaseInteractor {
 
-    fun doMyContributionRequest(contributionView: GitHubGraphView): Single<List<ContributionDay>>
+    fun doMyContributionRequest(): Single<List<ContributionDay>>
+
+    fun doContributionRequest(userName: String): Single<List<ContributionDay>>
+
+    fun loadFriends(): Observable<List<Friend>>
 
 }
--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractorImpl.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/friends/interactor/FriendsStatusInteractorImpl.kt
@@ -1,32 +1,38 @@
 package xyz.laziness.dailycommit.ui.modules.main.friends.interactor
 
+import io.reactivex.Observable
 import io.reactivex.Single
+import xyz.laziness.dailycommit.data.database.repository.friends.Friend
+import xyz.laziness.dailycommit.data.database.repository.friends.FriendRepo
 import xyz.laziness.dailycommit.data.network.github.GitHubApi
 import xyz.laziness.dailycommit.data.network.github.data.ContributionDay
 import xyz.laziness.dailycommit.data.parser.ContributionParser
 import xyz.laziness.dailycommit.data.preference.BasePreference
 import xyz.laziness.dailycommit.ui.base.interactor.BaseInteractorImpl
-import xyz.laziness.dailycommit.ui.custom.GitHubGraphView
 import xyz.laziness.dailycommit.utils.Colors
 import javax.inject.Inject
 
 
 class FriendsStatusInteractorImpl
-@Inject internal constructor(appPreference: BasePreference, apiHelper: GitHubApi): BaseInteractorImpl(appPreference, apiHelper), FriendsStatusInteractor {
+@Inject internal constructor(private val friendRepo: FriendRepo,
+                             appPreference: BasePreference,
+                             apiHelper: GitHubApi): BaseInteractorImpl(appPreference, apiHelper), FriendsStatusInteractor {
 
     private val baseBlockColor = Colors.baseColors[appPreference.getBaseBlockColor()]
 
-    override fun doMyContributionRequest(contributionView: GitHubGraphView): Single<List<ContributionDay>> {
-        contributionView.setBaseBlockColor(baseBlockColor)
-
-        return appPreference.getCurrentUserName().run {
-            apiHelper.doContributionRequest(this)
-                    .flatMapIterable { it.parse().select(ContributionParser.targetElement) }
-                    .filter { ContributionParser.hasAttributes(it) }
-                    .map { ContributionParser.parse(it) }
-                    .toList()
-        }
+    override fun doMyContributionRequest(): Single<List<ContributionDay>> {
+        ContributionParser.setBaseBlockColor(baseBlockColor)
+        return doContributionRequest(appPreference.getCurrentUserName())
     }
 
+    override fun doContributionRequest(userName: String): Single<List<ContributionDay>> =
+        apiHelper.doContributionRequest(userName)
+                .flatMapIterable { it.parse().select(ContributionParser.targetElement) }
+                .filter { ContributionParser.hasAttributes(it) }
+                .map { ContributionParser.parse(it) }
+                .toList()
+
+    override fun loadFriends(): Observable<List<Friend>> =
+            friendRepo.loadFriends(appPreference.getCurrentUserName())
 
 }
