19cde5a267e60877b61fcb9a1461cfe4ee63c966
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-03 22:59:41 +0900

    Modified glide version

--- app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
+++ app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
@@ -3,13 +3,13 @@ package xyz.laziness.dailycommit.utils.extensions
 import android.support.annotation.DrawableRes
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import xyz.laziness.dailycommit.R
 
 
 internal fun ImageView.loadImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_dashboard_black_24dp) {
     Glide.with(this.context)
             .load(url)
-            .apply(RequestOptions.placeholderOf(loadingImageRes).centerCrop())
+            .centerCrop()
+            .placeholder(loadingImageRes)
             .into(this)
 }
