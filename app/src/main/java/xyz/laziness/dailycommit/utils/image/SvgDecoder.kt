62d1141eb039d6ae5b55e52e4f209a2c29c856c3
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-03 23:30:10 +0900

    Add svgDecoder class

---
+++ app/src/main/java/xyz/laziness/dailycommit/utils/image/SvgDecoder.kt
@@ -0,0 +1,25 @@
+package xyz.laziness.dailycommit.utils.image
+
+import com.bumptech.glide.load.ResourceDecoder
+import com.bumptech.glide.load.engine.Resource
+import com.bumptech.glide.load.resource.SimpleResource
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGParseException
+import java.io.IOException
+import java.io.InputStream
+
+
+class SvgDecoder : ResourceDecoder<InputStream, SVG> {
+
+    override fun getId(): String = "SvgDecoder.xyz.laziness.dailycommit.app"
+
+    override fun decode(source: InputStream?, width: Int, height: Int): Resource<SVG> {
+        try {
+            val svg = SVG.getFromInputStream(source)
+            return SimpleResource<SVG>(svg)
+        } catch (ex: SVGParseException) {
+            throw IOException("Cannot load SVG from stream", ex)
+        }
+    }
+
+}
