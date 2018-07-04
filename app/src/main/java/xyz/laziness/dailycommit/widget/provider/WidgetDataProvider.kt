5fc7f51d59a99f8220e22779e65cb3d9b0ffac5a
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-05 00:19:32 +0900

    Add widgetData provider

---
+++ app/src/main/java/xyz/laziness/dailycommit/widget/provider/WidgetDataProvider.kt
@@ -0,0 +1,41 @@
+package xyz.laziness.dailycommit.widget.provider
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import com.bumptech.glide.GenericRequestBuilder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.model.StreamEncoder
+import com.bumptech.glide.load.resource.file.FileToStreamDecoder
+import com.caverock.androidsvg.SVG
+import xyz.laziness.dailycommit.data.network.github.GitHubApiConstants
+import xyz.laziness.dailycommit.data.preference.AppPreference
+import xyz.laziness.dailycommit.utils.AppConstants
+import xyz.laziness.dailycommit.utils.image.svg.SvgDecoder
+import xyz.laziness.dailycommit.utils.image.svg.SvgBitmapTranscoder
+import java.io.InputStream
+import javax.inject.Inject
+
+
+class WidgetDataProvider
+@Inject internal constructor(val appPreference: AppPreference) {
+
+    fun isLoggedIn(): Boolean = appPreference.getCurrentLoginState() != AppConstants.LoginState.LOGOUT.state
+
+    fun getMyContributionSVGBuilder(context: Context): GenericRequestBuilder<Uri, InputStream, SVG, Bitmap> =
+            getContributionSVGRequestBuilder(context, appPreference.getCurrentUserName())
+
+    private fun getContributionSVGRequestBuilder(context: Context, userName: String): GenericRequestBuilder<Uri, InputStream, SVG, Bitmap> =
+            Glide.with(context)
+                    .using(Glide.buildStreamModelLoader(Uri::class.java, context), InputStream::class.java)
+                    .from(Uri::class.java)
+                    .`as`(SVG::class.java)
+                    .transcode(SvgBitmapTranscoder(), Bitmap::class.java)
+                    .sourceEncoder(StreamEncoder())
+                    .cacheDecoder(FileToStreamDecoder<SVG>(SvgDecoder()))
+                    .decoder(SvgDecoder())
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .load(Uri.parse(GitHubApiConstants.CONTRIBUTION_URL.format(userName)))
+
+}
