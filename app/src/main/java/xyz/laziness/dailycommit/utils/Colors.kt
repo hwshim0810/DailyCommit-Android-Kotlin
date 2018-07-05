1ac14f912ef80c11d3fce4124842537c5607c88d
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-05 23:44:09 +0900

    Add Color themes

--- app/src/main/java/xyz/laziness/dailycommit/utils/Colors.kt
+++ app/src/main/java/xyz/laziness/dailycommit/utils/Colors.kt
@@ -4,9 +4,21 @@ package xyz.laziness.dailycommit.utils
 object Colors {
 
     const val DEFAULTS = "DEFAULTS"
+    const val CLASSIC = "CLASSIC"
+    const val OKINAWA_BLUE = "OKINAWA BLUE"
+    const val VELVET = "VELVET"
+    const val BLOOD_ORANGE = "BLOOD ORANGE"
+    const val DEEP_DARK = "DEEP DARK"
+
+    const val ZERO_COLOR = "#EBEDF0"
 
     val baseColors = hashMapOf(
-            DEFAULTS to listOfNotNull("#EBEDF0", "#239A3B", "#C6E48B", "7BC96F", "#196127")
+            DEFAULTS to listOfNotNull(ZERO_COLOR, "#239A3B", "#C6E48B", "7BC96F", "#196127"),
+            CLASSIC to listOfNotNull(ZERO_COLOR, "#CAC172", "#8F775C", "#DA8D40", "#2D130A"),
+            OKINAWA_BLUE to listOfNotNull(ZERO_COLOR, "#7EB2ED", "#4F7BC3", "#384B88", "#1C203C"),
+            VELVET to listOfNotNull(ZERO_COLOR, "#EE453F", "#C62D41", "#800E36", "#2A1729"),
+            BLOOD_ORANGE to listOfNotNull(ZERO_COLOR, "#C93C16", "#FD3919", "#FE8330", "#7D140B"),
+            DEEP_DARK to listOfNotNull(ZERO_COLOR, "#A5A5A5", "#707070", "#414141", "#141414")
     )
 
 }
