d5ec429a9fde11fa38bcc7a5e0320a8ef525951c
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-05 23:45:21 +0900

    Update pref setting to summary

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/settings/view/AppSettingFragment.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/settings/view/AppSettingFragment.kt
@@ -1,8 +1,12 @@
 package xyz.laziness.dailycommit.ui.modules.main.settings.view
 
 import android.os.Bundle
+import android.support.v7.preference.ListPreference
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
 import xyz.laziness.dailycommit.R
+import xyz.laziness.dailycommit.data.preference.AppPreference
+import xyz.laziness.dailycommit.utils.AppConstants
 
 
 class AppSettingPrefCompat : PreferenceFragmentCompat() {
@@ -16,7 +20,24 @@ class AppSettingPrefCompat : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceManager.sharedPreferencesName = AppConstants.PREF_NAME
         addPreferencesFromResource(R.xml.app_setting_pref)
+
+        setColorPrefSummary(AppPreference.PREF_KEY_BLOCK_COLOR)
+
+        PreferenceManager.getDefaultSharedPreferences(context).run {
+            registerOnSharedPreferenceChangeListener { _, key ->
+                setColorPrefSummary(key)
+            }
+        }
+    }
+
+    private fun setColorPrefSummary(key: String) {
+        val colorPref = findPreference(key)
+
+        if (colorPref is ListPreference)
+            colorPref.summary = colorPref.entry
+
     }
 
 }
