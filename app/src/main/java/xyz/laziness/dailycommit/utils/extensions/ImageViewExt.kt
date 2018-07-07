1c2858a020f6f0cdfbc170f3f2d22a835c34ecf4
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-07 13:01:07 +0900

    Add imageView ext method

--- app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
+++ app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
@@ -1,15 +1,26 @@
 package xyz.laziness.dailycommit.utils.extensions
 
 import android.support.annotation.DrawableRes
+import android.support.v4.widget.CircularProgressDrawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import xyz.laziness.dailycommit.R
 
 
-internal fun ImageView.loadImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_dashboard_black_24dp) {
+internal fun ImageView.loadImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_baseline_help_outline_24px) {
     Glide.with(this.context)
             .load(url)
             .centerCrop()
+            .error(R.drawable.ic_baseline_error_outline_24px)
             .placeholder(loadingImageRes)
+            .into(this)
+}
+
+internal fun ImageView.loadImage(url: String?, progress: CircularProgressDrawable) {
+    Glide.with(this.context)
+            .load(url)
+            .centerCrop()
+            .error(R.drawable.ic_baseline_error_outline_24px)
+            .placeholder(progress)
             .into(this)
 }
