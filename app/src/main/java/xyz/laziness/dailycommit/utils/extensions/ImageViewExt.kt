80adabd8a19ee1cba03aec1698a4fd4ee122d8db
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-08 00:50:32 +0900

    Add circle/rounded image load ext

--- app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
+++ app/src/main/java/xyz/laziness/dailycommit/utils/extensions/ImageViewExt.kt
@@ -4,23 +4,51 @@ import android.support.annotation.DrawableRes
 import android.support.v4.widget.CircularProgressDrawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.CropCircleTransformation
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 import xyz.laziness.dailycommit.R
 
 
-internal fun ImageView.loadImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_baseline_help_outline_24px) {
-    Glide.with(this.context)
-            .load(url)
+internal fun ImageView.loadImageBase(url: String?) =
+        Glide.with(this.context)
+                .load(url)
+                .error(R.drawable.ic_baseline_error_outline_24px)
+
+internal fun ImageView.loadImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_baseline_help_outline_24px) =
+        loadImageBase(url)
             .centerCrop()
-            .error(R.drawable.ic_baseline_error_outline_24px)
             .placeholder(loadingImageRes)
             .into(this)
-}
 
-internal fun ImageView.loadImage(url: String?, progress: CircularProgressDrawable) {
-    Glide.with(this.context)
-            .load(url)
-            .centerCrop()
-            .error(R.drawable.ic_baseline_error_outline_24px)
-            .placeholder(progress)
-            .into(this)
-}
+
+internal fun ImageView.loadImage(url: String?, progress: CircularProgressDrawable) =
+        loadImageBase(url)
+                .centerCrop()
+                .placeholder(progress)
+                .into(this)
+
+internal fun ImageView.loadCircleImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_baseline_help_outline_24px) =
+        loadImageBase(url)
+                .bitmapTransform(CropCircleTransformation(context))
+                .placeholder(loadingImageRes)
+                .into(this)
+
+
+internal fun ImageView.loadCircleImage(url: String?, progress: CircularProgressDrawable) =
+        loadImageBase(url)
+                .bitmapTransform(CropCircleTransformation(context))
+                .placeholder(progress)
+                .into(this)
+
+internal fun ImageView.loadRoundedCornerImage(url: String?, @DrawableRes loadingImageRes: Int = R.drawable.ic_baseline_help_outline_24px, radius: Int = 20) =
+        loadImageBase(url)
+                .bitmapTransform(RoundedCornersTransformation(context, radius, 0))
+                .placeholder(loadingImageRes)
+                .into(this)
+
+
+internal fun ImageView.loadRoundedCornerImage(url: String?, progress: CircularProgressDrawable, radius: Int = 20) =
+        loadImageBase(url)
+                .bitmapTransform(RoundedCornersTransformation(context, radius, 0))
+                .placeholder(progress)
+                .into(this)
