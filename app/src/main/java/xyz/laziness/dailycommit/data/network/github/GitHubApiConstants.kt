14cff938b236e2ceaaac2f8b07b2aaf4021d1abb
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-09 00:51:09 +0900

    Modified gitHub constants

--- app/src/main/java/xyz/laziness/dailycommit/data/network/github/GitHubApiConstants.kt
+++ app/src/main/java/xyz/laziness/dailycommit/data/network/github/GitHubApiConstants.kt
@@ -1,14 +1,14 @@
 package xyz.laziness.dailycommit.data.network.github
 
+import xyz.laziness.dailycommit.BuildConfig
+
 
 object GitHubApiConstants {
 
+    const val CLIENT_ID = BuildConfig.GITHUB_APP_ID
+
     const val BASE_URL = "https://github.com"
-    const val LOGIN_BASE_URL = "$BASE_URL/login/outh"
-    const val AUTHORIZE_URL = "$LOGIN_BASE_URL/authorize/"
-
     const val LOGIN_REDIRECT_URL = "dc://login"
-
     const val REST_URL = "https://api.github.com"
 
     const val REST_LOGIN_URL = "$REST_URL/authorizations"
@@ -16,5 +16,5 @@ object GitHubApiConstants {
     const val PUBLIC_USER_INFO_URL = "$REST_URL/users/%s"
     const val CONTRIBUTION_URL = "$BASE_URL/users/%s/contributions"
 
-    const val REQUST_SCOPE = "user repo"
+    const val REQUST_SCOPE = "user,repo"
 }
