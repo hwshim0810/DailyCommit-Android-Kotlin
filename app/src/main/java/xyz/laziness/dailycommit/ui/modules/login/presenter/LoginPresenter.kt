656a4a6b45227c4423c2fb6f9774f3b11ce29527
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-09 23:58:16 +0900

    Add oauth intent handler

--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/presenter/LoginPresenter.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/presenter/LoginPresenter.kt
@@ -1,5 +1,6 @@
 package xyz.laziness.dailycommit.ui.modules.login.presenter
 
+import android.content.Intent
 import xyz.laziness.dailycommit.ui.base.presenter.BasePresenter
 import xyz.laziness.dailycommit.ui.modules.login.interactor.LoginInteractor
 import xyz.laziness.dailycommit.ui.modules.login.view.LoginView
@@ -8,5 +9,7 @@ import xyz.laziness.dailycommit.ui.modules.login.view.LoginView
 interface LoginPresenter<V: LoginView, I: LoginInteractor> : BasePresenter<V, I> {
 
     fun setLoginMainDisplay(): Unit?
+
+    fun onHandleOauthIntent(intent: Intent?)
 
 }
--- app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/presenter/LoginPresenterImpl.kt
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/login/presenter/LoginPresenterImpl.kt
@@ -1,10 +1,17 @@
 package xyz.laziness.dailycommit.ui.modules.login.presenter
 
+import android.content.Intent
+import android.util.Log
+import com.androidnetworking.error.ANError
 import io.reactivex.disposables.CompositeDisposable
+import xyz.laziness.dailycommit.data.network.github.GitHubApiConstants
+import xyz.laziness.dailycommit.data.network.github.response.OauthTokenResponse
 import xyz.laziness.dailycommit.ui.base.presenter.BasePresenterImpl
 import xyz.laziness.dailycommit.ui.modules.login.interactor.LoginInteractor
 import xyz.laziness.dailycommit.ui.modules.login.view.LoginView
+import xyz.laziness.dailycommit.utils.AppConstants
 import xyz.laziness.dailycommit.utils.io.SchedulerHelper
+import java.net.HttpURLConnection
 import javax.inject.Inject
 
 
@@ -22,4 +29,38 @@ class LoginPresenterImpl<V: LoginView, I: LoginInteractor>
 
     override fun setLoginMainDisplay() = getView()?.openLoginChooseFragment()
 
+    override fun onHandleOauthIntent(intent: Intent?) {
+        intent?.takeIf { it.data != null }?.run {
+            val uri = this.data
+
+            if (uri.toString().startsWith(GitHubApiConstants.LOGIN_REDIRECT_URL)) {
+                val code = uri.getQueryParameter("code")
+                if (code.isNotEmpty()) doOauthLoginCall(code)
+            }
+        }
+    }
+
+    private fun doOauthLoginCall(code: String) {
+        interactor?.let {
+            compositeDisposable.add(
+                    it.doOauthAccessTokenCall(code)
+                            .compose(schedulerHelper.ioToMainSingleScheduler())
+                            .subscribe({
+                                updateLoginInfoInPreference(it, AppConstants.LoginMethod.WEB)
+                                getView()?.startMainActivity()
+                            }, {
+                                it as ANError
+                                Log.d("EE", it.message)
+                                if (it.errorCode == HttpURLConnection.HTTP_UNAUTHORIZED)
+                                    getView()?.showLoginError(AppConstants.LOGIN_FAILURE_ERROR)
+                                else
+                                    this.onError()
+
+                            })
+            )
+        }
+    }
+
+    private fun updateLoginInfoInPreference(tokenResponse: OauthTokenResponse, loginMethod: AppConstants.LoginMethod) =
+        interactor?.updateLoginInfoInPreference(tokenResponse, loginMethod)
 }
