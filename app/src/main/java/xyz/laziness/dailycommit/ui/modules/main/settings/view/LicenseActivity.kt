d064d5155c7daf73af7a0f937716663c17e53cf7
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-12 00:06:39 +0900

    Add openSource license activity

---
+++ app/src/main/java/xyz/laziness/dailycommit/ui/modules/main/settings/view/LicenseActivity.kt
@@ -0,0 +1,14 @@
+package xyz.laziness.dailycommit.ui.modules.main.settings.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import xyz.laziness.dailycommit.R
+
+
+class LicenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+    }
+}
